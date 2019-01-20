884fc2c8fde7d431e736a64e925b0fa87b7e791b
rakumar <rakumar@ptc.com>
2019-01-20 23:18:19 +0530

    changed server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/movie-analysis'));
 app.listen(process.env.PORT || 8080, function () {
   console.log("Server is up and running");
 });
